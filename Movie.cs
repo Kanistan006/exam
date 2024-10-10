9705950b3950b3256f9b88c846ac726d70cf3f18
Kanistan T. <UT01265@unicomtic.com>
2024-10-10 08:57:39 +0530

    override

--- Movie.cs
+++ Movie.cs
@@ -12,5 +12,19 @@ namespace MovieRentalManagementSystem
         public string Title { get; set; }
         public string Director { get; set; }
         public decimal RentalPrice { get; set; }
+
+
+        public Movie(int movieId, string title, string director, decimal rentalPrice)
+        {
+            MovieId = movieId;
+            Title = title;
+            Director = director;
+            RentalPrice = rentalPrice;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {MovieId}, Title: {Title}, Director: {Director}, RentalPrice: {RentalPrice}";
+        }
     }
 }
